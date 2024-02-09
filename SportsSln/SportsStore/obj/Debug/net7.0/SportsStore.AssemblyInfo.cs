17c2b17dd18be1b32acf58887c3175d80bea57ed
Gissel Santoyo <gisselsantoyo@mail.weber.edu>
2024-02-09 09:40:23 -0700

    ss-9 complete

--- SportsSln/SportsStore/obj/Debug/net7.0/SportsStore.AssemblyInfo.cs
+++ SportsSln/SportsStore/obj/Debug/net7.0/SportsStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SportsStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fd7f99170e82c02bb6c244983824b5c42848278")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ebc3d0aed65d84e5e6c88c52c5cf17381efc796")]
 [assembly: System.Reflection.AssemblyProductAttribute("SportsStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SportsStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
